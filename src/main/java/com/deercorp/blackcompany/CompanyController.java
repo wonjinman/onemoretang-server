f69d8d2bceec0b59f1685e9c3aab495b1da9eb19
deer-chiho <chiho.kim@deering.co>
2023-06-30 14:03:52 +0900

    fix: Round the average rating to the first decimal place

--- src/main/java/com/deercorp/blackcompany/CompanyController.java
+++ src/main/java/com/deercorp/blackcompany/CompanyController.java
@@ -18,20 +18,20 @@ public class CompanyController {
         List<CompanyEntity> companyEntityList = companyRepository.findTop10ByNameContaining(query);
         // 회사 리뷰 가져오기
         List<CompanyResponse> companyResponses = companyEntityList.stream()
-            .map(entity -> {
-                List<CompanyReviewEntity> companyReviews = companyReviewRepository.findAllByCompanyUuid(entity.getUuid());
-                return new CompanyResponse(
-                    entity.getUuid(),
-                    entity.getName(),
-                    entity.getAddress(),
-                    entity.getRepresentativeName(),
-                    entity.getContact(),
-                    entity.getSubContact(),
-                    entity.getRegion(),
-                    (long) companyReviews.size(),
-                    getAverageRating(companyReviews)
-                );
-            }).toList();
+                .map(entity -> {
+                    List<CompanyReviewEntity> companyReviews = companyReviewRepository.findAllByCompanyUuid(entity.getUuid());
+                    return new CompanyResponse(
+                            entity.getUuid(),
+                            entity.getName(),
+                            entity.getAddress(),
+                            entity.getRepresentativeName(),
+                            entity.getContact(),
+                            entity.getSubContact(),
+                            entity.getRegion(),
+                            (long) companyReviews.size(),
+                            getAverageRating(companyReviews)
+                    );
+                }).toList();
         return CommonResponse.ok(new CompaniesResponse(companyResponses));
     }
 
@@ -40,30 +40,30 @@ public class CompanyController {
         CompanyEntity companyEntity = companyRepository.findByUuid(companyUuid).orElseThrow(IllegalArgumentException::new);
         List<CompanyReviewEntity> companyReviewEntityList = companyReviewRepository.findAllByCompanyUuid(companyUuid);
         List<CompanyReviewResponse> companyReviewResponseList = companyReviewEntityList.stream()
-            .map(entity ->
-                new CompanyReviewResponse(
-                    entity.getUuid(),
-                    entity.getContent(),
-                    entity.getRating(),
-                    entity.getCreatedAt(),
-                    entity.getPaymentLeadTime(),
-                    entity.getRegion()
-                )
-            ).toList();
+                .map(entity ->
+                        new CompanyReviewResponse(
+                                entity.getUuid(),
+                                entity.getContent(),
+                                entity.getRating(),
+                                entity.getCreatedAt(),
+                                entity.getPaymentLeadTime(),
+                                entity.getRegion()
+                        )
+                ).toList();
 
         return CommonResponse.ok(
-            new CompanyWithReviewResponse(
-                companyEntity.getUuid(),
-                companyEntity.getName(),
-                companyEntity.getAddress(),
-                companyEntity.getRepresentativeName(),
-                companyEntity.getContact(),
-                companyEntity.getSubContact(),
-                companyEntity.getRegion(),
-                (long) companyReviewEntityList.size(),
-                getAverageRating(companyReviewEntityList),
-                companyReviewResponseList
-            )
+                new CompanyWithReviewResponse(
+                        companyEntity.getUuid(),
+                        companyEntity.getName(),
+                        companyEntity.getAddress(),
+                        companyEntity.getRepresentativeName(),
+                        companyEntity.getContact(),
+                        companyEntity.getSubContact(),
+                        companyEntity.getRegion(),
+                        (long) companyReviewEntityList.size(),
+                        getAverageRating(companyReviewEntityList),
+                        companyReviewResponseList
+                )
         );
     }
 
@@ -75,20 +75,20 @@ public class CompanyController {
     @PostMapping("/reviews")
     public void postReview(@RequestHeader("x-device-key") String deviceKey, @RequestBody PostReviewRequest postReviewRequest) {
         CompanyReviewEntity entity = new CompanyReviewEntity(
-            deviceKey,
-            postReviewRequest.getCompanyUuid(),
-            postReviewRequest.getRating(),
-            postReviewRequest.getContent(),
-            postReviewRequest.getPaymentLeadTime(),
-            postReviewRequest.getRegion()
+                deviceKey,
+                postReviewRequest.getCompanyUuid(),
+                postReviewRequest.getRating(),
+                postReviewRequest.getContent(),
+                postReviewRequest.getPaymentLeadTime(),
+                postReviewRequest.getRegion()
         );
         companyReviewRepository.save(entity);
     }
 
     private static double getAverageRating(List<CompanyReviewEntity> companyReviews) {
-        return companyReviews.stream()
-            .mapToDouble(CompanyReviewEntity::getRating)
-            .average()
-            .orElse(0.0);
+        return Math.floor(companyReviews.stream()
+                .mapToDouble(CompanyReviewEntity::getRating)
+                .average()
+                .orElse(0.0) * 10) / 10;
     }
 }
